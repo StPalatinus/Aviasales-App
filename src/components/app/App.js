968bdc68097f7e37324cc75b60295420c367f5fd
StPalatinus <PalaCisco@gmail.com>
2021-06-07 06:08:47 +0300

    changed babel-eslint to babe/eslin-parser

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,21 +2,21 @@ import "./App.scss";
 import React from "react";
 import mainLogo, { ReactComponent as Logo } from "../../img/mainlogo.svg";
 
-const logo = require("../../img/mainlogo.svg");
+// const logo = require('../../img/mainlogo.svg');
 
 function App() {
   console.log("import:");
   console.log(mainLogo);
-  console.log("require");
-  console.log(logo);
-  console.log(logo.default);
+  // console.log("require");
+  // console.log(logo);
+  // console.log(logo.default);
   return (
     <section className="App">
       <header>
         <img className="header--logo" srs={mainLogo} alt="mainlogo" />
-        <img className="header--logo" srs={logo} alt="logo" />
-        <img src={`${process.env.PUBLIC_URL}/mainlogo.svg`} alt="logo" />
         <Logo />
+        {/* <img className="header--logo" srs={logo} alt="logo"/> */}
+        {/* <img src={`${process.env.PUBLIC_URL  }/mainlogo.svg`} alt="logo" /> */}
       </header>
       <aside>_ASIDE_</aside>
       <main>_BODY_</main>
